03646e10dbdc730e3d3aad755d54c1378cd173e6
Kevin Baugh <kbaugh@indeed.com>
2018-09-23 12:17:09 -0500

    Javascript isn't working on prod :/

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('../sw.js')
+        .register('sw.js')
         .then(reg => console.log('Service Worker is now registered'))
         .catch(err => console.log(`Service Worker: Error: ${err}`))
     })
